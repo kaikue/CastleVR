9c4e2e77600b1f14090b162d8e2a3f336591cd08
IDeATe Laptop <kkuehner@andrew.cmu.edu>
2019-02-01 15:32:34 -0500

    spear stuff

---
+++ Assets/Scripts/SpearSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearSpawner : MonoBehaviour
+{
+    public GameObject spearPrefab;
+    int spears = 0;
+
+    void Start()
+    {
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        if (spears == 0)
+        {
+            SpawnSpear();
+        }
+        spears = 0;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+    private void SpawnSpear()
+    {
+        //TODO
+        Instantiate(spearPrefab);
+    }
+}
