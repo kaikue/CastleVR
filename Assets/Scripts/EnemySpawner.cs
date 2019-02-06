26c88907ca9cbed08ecb05559962ac7552eb94a0
Vidhart Bhatia <vidhartbhatia@hotmail.com>
2019-02-06 16:29:50 -0500

    vids bow and arrow working

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+      
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        GameObject temp = Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        EnemyWalk script = temp.GetComponent<EnemyWalk>();
+
+
+        float xVel = Random.Range(1.0f, 5.0f);
+        script.setVelocity(new Vector3(xVel, 0, 0));
+        //script.velocity = new Vector3(xVel, 0, 0);
+
+    }
+}
