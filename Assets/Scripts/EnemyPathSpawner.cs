54c3f9443b4540fafb0f879f234ed6df0afd2085
kaikue <kvk@pobox.com>
2019-02-22 15:21:57 -0500

    bezier curves

---
+++ Assets/Scripts/EnemyPathSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyPathSpawner : MonoBehaviour
+{
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+
+	private BezierCurve curve;
+
+    void Start()
+    {
+		curve = GetComponent<BezierCurve>();
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        GameObject temp = Instantiate(enemy, curve.GetPointAt(0), Quaternion.identity);
+        EnemyPathWalk script = temp.GetComponent<EnemyPathWalk>();
+		script.SetCurve(curve);
+    }
+}
