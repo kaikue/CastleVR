0038de98d9d8ac646fc5cd6863eb043bd940b51e
tvondavi <tvondavi@andrew.cmu.edu>
2019-02-25 19:22:05 -0500

    more additions to integrate VR controls

---
+++ Assets/Scripts/SnapDropScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapDropScript : MonoBehaviour
+{
+    public GameObject insertBall;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == insertBall)
+        {
+            TurnOffScript();
+        }
+    }
+
+    void TurnOffScript()
+    {
+        insertBall.GetComponent<Interactable>().enabled = false;
+    }
+}
