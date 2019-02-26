544dbcc7d9100de1e8d73690deefe268f811dd87
tvondavi <tvondavi@andrew.cmu.edu>
2019-02-26 18:30:58 -0500

    Edits and updated unity

--- Assets/Scripts/SnapDropScript.cs
+++ Assets/Scripts/SnapDropScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SnapDropScript : MonoBehaviour
 {
-    public GameObject insertBall;
+    /*public GameObject insertBall;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,5 +17,5 @@ public class SnapDropScript : MonoBehaviour
     void TurnOffScript()
     {
         insertBall.GetComponent<Interactable>().enabled = false;
-    }
+    }*/
 }
