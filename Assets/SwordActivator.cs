fd892f4a1cf1bdcee64fd938662c75d2db4d7eda
IDeATe Laptop <kkuehner@andrew.cmu.edu>
2019-02-27 16:56:23 -0500

    sword

---
+++ Assets/SwordActivator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordActivator : MonoBehaviour
+{
+    public GameObject swordPrefab;
+    public Transform swordParent;
+
+    private GameObject swordObj;
+
+    public void Activate()
+    {
+        swordObj = Instantiate(swordPrefab, swordParent);
+    }
+
+    public void Deactivate()
+    {
+        Destroy(swordObj);
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Castle VR
 
 A game for Windows Mixed Reality by Studio Lighthouse
+
+## Packages Used
+- https://assetstore.unity.com/packages/3d/characters/toony-tiny-ww1-soldiers-demo-115694
