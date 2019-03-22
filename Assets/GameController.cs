193ba66d33c055e7f956e00a0a435dbc42f6662c
Vidhart Bhatia <vidhartbhatia@hotmail.com>
2019-03-22 14:45:28 -0400

    waves
    basic waves spawn

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour { 
 
-    public TextMeshPro txt;
+    public TextMeshPro score_txt;
+    public TextMeshPro wave_txt;
     public int enemies_spawned;
     public int enemies_through;
+    public int wave = 0;
 
 
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (score_txt == null)
+        {
+            GameObject score = GameObject.Find("Score_Text");
+            score_txt = score.GetComponent<TextMeshPro>();
+        }
+
+        if (wave_txt == null)
+        {
+            GameObject wave = GameObject.Find("Wave_Text");
+            wave_txt = wave.GetComponent<TextMeshPro>();
+        }
+    }
     void Start()
     {
-        txt.text = "hello";
-        
+       
+
+
     }
 
     // Update is called once per frame
@@ -22,6 +40,7 @@ public class GameController : MonoBehaviour {
     {
         
     }
+
     public void add_to_enemies_spawned(int x)
     {
         enemies_spawned += x;
@@ -36,6 +55,18 @@ public class GameController : MonoBehaviour {
 
     void UpdateScore()
     {
-        txt.text = "Spawned: " + enemies_spawned + " Let Through: " + enemies_through;
+        score_txt.text = "Spawned: " + enemies_spawned + " Let Through: " + enemies_through;
     }
+
+    public void increase_wave()
+    {
+        wave += 1;
+        UpdateWave();
+    }
+
+    void UpdateWave()
+    {
+        wave_txt.text = "Wave: " + wave;
+    }
+
 }
--- Assets/Scripts/EnemyPathSpawner.cs
+++ Assets/Scripts/EnemyPathSpawner.cs
@@ -6,29 +6,54 @@ using UnityEngine;
 public class EnemyPathSpawner : MonoBehaviour
 {
     public GameObject[] enemies;                // The enemy prefab to be spawned.
-    public float spawnTime = 3f;            // How long between each spawn.
+    public float spawnTime = 3f;  // How long between each spawn.
+    public float wave_time = 5f; // time between waves
+    public int spawn_time = 2;
 
 	private BezierCurve curve;
     public GameController gc;
 
-    void Start()
+    public int num_enemies = 5;
+
+    private void Start()
     {
         GameObject gco = GameObject.Find("GameController");
         gc = gco.GetComponent<GameController>();
         curve = GetComponent<BezierCurve>();
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
-
+        //InvokeRepeating("Spawn", spawnTime, spawnTime);
+        //InvokeRepeating("SpawnWave", wave_time, wave_time);
+        StartCoroutine("SpawnWave");
 
     }
 
-
-    void Spawn()
+    private void SpawnRandomEnemy()
     {
         GameObject temp = Instantiate(enemies[Random.Range(0,enemies.Length)], curve.GetPointAt(0), Quaternion.identity);
         EnemyPathWalk script = temp.GetComponent<EnemyPathWalk>();
-
         gc.add_to_enemies_spawned(1);
 		script.SetCurve(curve);
+    }
+
+    IEnumerator SpawnWave()
+    {
+        //yield return new WaitForSeconds(wave_time);
+
+        gc.increase_wave();
+
+        for (int i = 0; i< num_enemies; i++)
+        {
+            SpawnRandomEnemy();
+
+            // wait a random amoount of time between spawining enemenies
+            int wait = Random.Range(spawn_time, spawn_time+5);
+            yield return new WaitForSeconds(wait);
+        }
+
+        num_enemies *= 2;
+
+        yield return new WaitForSeconds(wave_time);
+        StartCoroutine("SpawnWave");
+
     }
 }
