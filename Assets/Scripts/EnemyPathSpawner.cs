7872bb1c4734c2c1f40e28ebaa912385df47653a
Vidhart Bhatia <vidhartbhatia@hotmail.com>
2019-03-27 17:28:36 -0400

    changing waves

--- Assets/Scripts/EnemyPathSpawner.cs
+++ Assets/Scripts/EnemyPathSpawner.cs
@@ -5,55 +5,70 @@ using UnityEngine;
 
 public class EnemyPathSpawner : MonoBehaviour
 {
-    public GameObject[] enemies;                // The enemy prefab to be spawned.
-    public float spawnTime = 3f;  // How long between each spawn.
-    public float wave_time = 5f; // time between waves
-    public int spawn_time = 2;
+    //   public GameObject[] enemies;                // The enemy prefab to be spawned.
+    //   public float spawnTime = 3f;  // How long between each spawn.
+    //   public float wave_time = 5f; // time between waves
+    //   public int spawn_time = 2;
 
-	private BezierCurve curve;
-    public GameController gc;
+    private BezierCurve curve;
+    //   public GameController gc;
 
-    public int num_enemies = 5;
+    //   public int num_enemies = 5;
 
-    private void Start()
+    private void Awake()
     {
-        GameObject gco = GameObject.Find("GameController");
-        gc = gco.GetComponent<GameController>();
+        //GameObject gco = GameObject.Find("GameController");
+        //gc = gco.GetComponent<GameController>();
         curve = GetComponent<BezierCurve>();
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
         //InvokeRepeating("Spawn", spawnTime, spawnTime);
         //InvokeRepeating("SpawnWave", wave_time, wave_time);
-        StartCoroutine("SpawnWave");
+  
+        //StartCoroutine("SpawnWave");
 
     }
 
-    private void SpawnRandomEnemy()
+  //  private void SpawnRandomEnemy()
+  //  {
+  //      GameObject temp = Instantiate(enemies[Random.Range(0,enemies.Length)], curve.GetPointAt(0), Quaternion.identity);
+  //      EnemyPathWalk script = temp.GetComponent<EnemyPathWalk>();
+  //      gc.add_to_enemies_spawned(1);
+		//script.SetCurve(curve);
+  //  }
+
+
+        // spawns an enemy of the sent prefab at this curve
+
+    public void SpawnEnemy(GameObject enemy)
     {
-        GameObject temp = Instantiate(enemies[Random.Range(0,enemies.Length)], curve.GetPointAt(0), Quaternion.identity);
+
+        //print(curve);
+        GameObject temp = Instantiate(enemy, curve.GetPointAt(0), Quaternion.identity);
         EnemyPathWalk script = temp.GetComponent<EnemyPathWalk>();
-        gc.add_to_enemies_spawned(1);
-		script.SetCurve(curve);
-    }
+        script.SetCurve(curve);
 
-    IEnumerator SpawnWave()
-    {
-        //yield return new WaitForSeconds(wave_time);
-
-        gc.increase_wave();
-
-        for (int i = 0; i< num_enemies; i++)
-        {
-            SpawnRandomEnemy();
-
-            // wait a random amoount of time between spawining enemenies
-            int wait = Random.Range(spawn_time, spawn_time+5);
-            yield return new WaitForSeconds(wait);
-        }
-
-        num_enemies *= 2;
-
-        yield return new WaitForSeconds(wave_time);
-        StartCoroutine("SpawnWave");
 
     }
+
+    //    IEnumerator SpawnWave()
+    //    {
+    //        //yield return new WaitForSeconds(wave_time);
+
+    //        gc.increase_wave();
+
+    //        for (int i = 0; i< num_enemies; i++)
+    //        {
+    //            SpawnRandomEnemy();
+
+    //            // wait a random amoount of time between spawining enemenies
+    //            int wait = Random.Range(spawn_time, spawn_time+5);
+    //            yield return new WaitForSeconds(wait);
+    //        }
+
+    //        num_enemies *= 2;
+
+    //        yield return new WaitForSeconds(wave_time);
+    //        StartCoroutine("SpawnWave");
+
+    //    }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,10 +7,19 @@ public class GameController : MonoBehaviour {
 
     public TextMeshPro score_txt;
     public TextMeshPro wave_txt;
-    public int enemies_spawned;
+    public int enemies_left_to_spawn;
     public int enemies_through;
     public int wave = 0;
 
+
+    public GameObject[] enemies;    // The enemy prefabs to be spawned.
+    public float spawnTime = 3f;  // How long between each spawn.
+    public float wave_time = 5f; // time between waves
+    public int spawn_time = 10;
+
+    public EnemyPathSpawner[] enemyPaths;
+
+    public int num_enemies = 5;
 
     // Start is called before the first frame update
 
@@ -30,7 +39,10 @@ public class GameController : MonoBehaviour {
     }
     void Start()
     {
-       
+
+
+        StartCoroutine(SpawnWaves(0));
+        //StartCoroutine(SpawnWaves(1));
 
 
     }
@@ -41,11 +53,20 @@ public class GameController : MonoBehaviour {
         
     }
 
-    public void add_to_enemies_spawned(int x)
+    public void subtract_from_enemies_left(int x)
     {
-        enemies_spawned += x;
+        enemies_left_to_spawn -= x;
         UpdateScore();
     }
+
+    public void reset_enemies_left(int x)
+    {
+        enemies_left_to_spawn = x;
+        UpdateScore();
+    }
+
+
+
 
     public void add_to_enemies_through(int x)
     {
@@ -55,7 +76,7 @@ public class GameController : MonoBehaviour {
 
     void UpdateScore()
     {
-        score_txt.text = "Spawned: " + enemies_spawned + " Let Through: " + enemies_through;
+        score_txt.text = "Incoming: " + enemies_left_to_spawn + " Let Through: " + enemies_through;
     }
 
     public void increase_wave()
@@ -69,4 +90,35 @@ public class GameController : MonoBehaviour {
         wave_txt.text = "Wave: " + wave;
     }
 
+
+
+
+    IEnumerator SpawnWaves(int path_index)
+    {
+        //yield return new WaitForSeconds(wave_time);
+
+        increase_wave();
+        reset_enemies_left(num_enemies);
+
+        for (int i = 0; i < num_enemies; i++)
+        {
+            // call spawnEnmenty on a path
+            EnemyPathSpawner path = enemyPaths[Random.Range(0, enemyPaths.Length)];
+            GameObject enemy_prefab = enemies[Random.Range(0, enemies.Length)];
+            path.SpawnEnemy(enemy_prefab);
+            subtract_from_enemies_left(1);
+
+            // wait a random amoount of time between spawining enemenies
+            int wait = Random.Range(spawn_time, spawn_time + 5);
+            yield return new WaitForSeconds(wait);
+        }
+
+        num_enemies += 10;
+
+        yield return new WaitForSeconds(wave_time);
+        StartCoroutine(SpawnWaves(0));
+
+    }
 }
+
+
