a3826da71b66b423a40d2849380171af1692ee11
tvondavi <tvondavi@andrew.cmu.edu>
2019-03-29 16:02:04 -0400

    thomas's edits to Catapult lever

---
+++ Assets/Scripts/LeverLaunch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace Vale.VR.InteractionSystem
+{
+    public class LeverLaunch : MonoBehaviour
+    {
+        public LinearMapping linearMapping;
+
+        private float currentLinearMapping;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (currentLinearMapping != linearMapping.value)
+            {
+                currentLinearMapping = linearMapping.value;
+                Debug.Log(currentLinearMapping);
+            }
+        }
+    }
+}
