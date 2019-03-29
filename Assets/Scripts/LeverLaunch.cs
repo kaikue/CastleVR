69648ce74c61ce9ef4413ca16e400a808f518048
tvondavi <tvondavi@andrew.cmu.edu>
2019-03-29 16:22:54 -0400

    more catapult controls

--- Assets/Scripts/LeverLaunch.cs
+++ Assets/Scripts/LeverLaunch.cs
@@ -9,15 +9,79 @@ namespace Vale.VR.InteractionSystem
     {
         public LinearMapping linearMapping;
 
-        private float currentLinearMapping;
+        public Rigidbody ball;
+        private Transform target;
+        public GameObject trajectoryPointPrefab;
+        //public GameObject ballPrefab;
+        public LayerMask clickMask;
+
+        private LineRenderer line;
+        private Transform launcher;
+
+        public bool debugPath;
+
+        public float h = 30;
+        public float gravity = -18;
 
         // Update is called once per frame
         void Update()
         {
-            if (currentLinearMapping != linearMapping.value)
+            if (linearMapping.value == 1.0f)
             {
-                currentLinearMapping = linearMapping.value;
-                Debug.Log(currentLinearMapping);
+                Launch();
+            }
+
+            if (debugPath)
+            {
+                DrawPath();
+            }
+        }
+
+
+        void Launch()
+        {
+            Physics.gravity = Vector3.up * gravity;
+            ball.useGravity = true;
+            ball.velocity = CalculateLaunchData().initialVelocity;
+            print(CalculateLaunchData().initialVelocity);
+        }
+
+        LaunchData CalculateLaunchData()
+        {
+            float displacementY = target.position.y - ball.position.y;
+            Vector3 displacementXZ = new Vector3(target.position.x - ball.position.x, 0, target.position.z - ball.position.z);
+            float time = Mathf.Sqrt(-2 * h / gravity) + Mathf.Sqrt(2 * (displacementY - h) / gravity);
+            Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * h);
+            Vector3 velocityXZ = displacementXZ / time;
+
+            return new LaunchData(velocityXZ + velocityY, time);
+        }
+
+        void DrawPath()
+        {
+            LaunchData launchData = CalculateLaunchData();
+            Vector3 previousDrawPoint = ball.position;
+            int resolution = 30;
+
+            for (int i = 1; i <= resolution; i++)
+            {
+                float simulationTime = i / (float)resolution * launchData.timeToTarget;
+                Vector3 displacement = launchData.initialVelocity * simulationTime + Vector3.up * gravity * simulationTime * simulationTime / 2f;
+                Vector3 drawPoint = ball.position + displacement;
+                Debug.DrawLine(previousDrawPoint, drawPoint, Color.green);
+                previousDrawPoint = drawPoint;
+            }
+        }
+
+        struct LaunchData
+        {
+            public readonly Vector3 initialVelocity;
+            public readonly float timeToTarget;
+
+            public LaunchData(Vector3 initialVelocity, float timeToTarget)
+            {
+                this.initialVelocity = initialVelocity;
+                this.timeToTarget = timeToTarget;
             }
         }
     }
